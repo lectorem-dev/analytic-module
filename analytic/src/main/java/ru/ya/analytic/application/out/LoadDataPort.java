1dc02f9c2c83f747ef2bc067962fb41258171a89
lectorem-dev <79637710149@mail.com>
2025-11-25 18:59:12 +0300

    Add AnalyticsPort and LoadDataPort accidentally ignored before

---
+++ analytic/src/main/java/ru/ya/analytic/application/out/LoadDataPort.java
@@ -0,0 +1,11 @@
+package ru.ya.analytic.application.out;
+
+import ru.ya.analytic.adapters.in.model.ReferedEvent;
+import ru.ya.analytic.adapters.in.model.RequestedEvent;
+
+import java.util.List;
+
+public interface LoadDataPort {
+    boolean loadRequested(List<RequestedEvent> events);
+    boolean loadReferred(List<ReferedEvent> events);
+}
